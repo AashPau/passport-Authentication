b47f0a683b601537a7c86b12ebec9e85a807daf0
Aash <31497659+AashPau@users.noreply.github.com>
2024-04-28 10:52:28 +1000

    feat: setup routes and expresslayouts

--- Script.js
+++ Script.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+app.use("/", require("./Routes/index"));
+app.use("/users", require("./Routes/users"));
 
 app.listen(PORT, console.log(`Server started at http://localhost:${PORT}`));
